0ff8caeb14b9b66760488b06a7301a15780550a0
azwolff <ethanwolffaz@gmail.com>
2024-07-06 13:43:17 -0700

    Finished breed(), added more stylng/formatting. Need to polish add_xp

--- breeder.py
+++ breeder.py
@@ -9,6 +9,7 @@ class Gecko:
     self.can_breed = can_breed
     self.is_hungry = is_hungry
     gecko_collection.append(self)
+    player_1.add_xp()
 
   def __repr__(self):
     description = "{name} is a {sex} gecko and has the {morph} morphotype.".format(name = self.name, sex = self.sex, morph = self.morph)
@@ -21,12 +22,6 @@ class Gecko:
     else:
       description += " It's not hungry right now."
     return description
-
-  # this method will start a cooldown period after a female has been
-  # bred. After the female breeds, can_breed = False. 
-  # After 3 breeding attempts, can_breed = True.
-  def cooldown(self, other_gecko):
-    pass
 
 gecko_collection = []
 
@@ -45,8 +40,6 @@ exotic_morph_xp = []
 for i in range(len(exotic_morph_list) + 1):
   exotic_morph_xp.append(10)
 exotic_xp = dict(zip(exotic_morph_list, exotic_morph_xp))
-
-#print(exotic_xp)
 
 basic_morph_list = ["normal","reduced pattern", "powder blue", "patternless", "piebald", "candy dot",  "caramel", "striped", "zigzag"]
 
@@ -70,13 +63,8 @@ class Player:
       player_desc += " geckos.\n"
     return player_desc
 
-  # breed() will display a list of the female and male geckos in the player's collection. 
-  # Then player will select one male and one female from the collection. 
-  # The function will take an input for the geckos name and search gecko_collection for the name. 
-  # If in the list, breed() will check if can_breed = True. 
-  # If both male and female can_breed = True, breed() will create a new instance of Gecko() with a random generated name, sex, and morph.
-  #  Depending on the morph generated, morph xp will be added to player xp. After the geckos successfully breed, the can_breed = False. 
-  # If a gecko has not been fed since instantiation, the gecko will need to fed to be breedable. 
+# I need to make the xp funtion work correctly
+ 
   def breed(self):
     print("Choose a female gecko: ")
     for gecko in gecko_collection:
@@ -85,7 +73,7 @@ class Player:
     female_selection = input("Enter female gecko name: ")
     for gecko in gecko_collection:
         if female_selection == gecko.name and gecko.can_breed == False:
-          print("This gecko isn't ready to breed right now. Please choose another.\n")
+          print("This gecko isn't ready to breed right now. Please feed it or choose another.\n")
         elif female_selection == gecko.name and gecko.can_breed == True:
           print("Choose a male gecko: ")
           for gecko in gecko_collection:
@@ -94,11 +82,12 @@ class Player:
           male_selection = input("Enter male gecko name: ")
           for gecko in gecko_collection:
             if male_selection == gecko.name and gecko.can_breed == False:
-              print("This gecko isn't ready to breed right now. Please choose another.\n")
+              print("This gecko isn't ready to breed right now. Please feed it or choose another.\n")
             elif male_selection == gecko.name and gecko.can_breed == True:
-              Gecko(random.choice(names_list), random.choice(sex), random.choice(all_morph_list))
-              print("""Congratulations!!!!
-                    2 new geckos hatched and were added to your collection!""")
+              new_gecko = Gecko(random.choice(names_list), random.choice(sex), random.choice(all_morph_list))
+              print("Congratulations!!!!\n A new gecko hatched and was added to your collection!")
+              gecko.can_breed == False
+              gecko.is_hungry == True
         
 # feed() is working    
   def feed(self):
@@ -111,18 +100,22 @@ class Player:
       if gecko.name == needs_food:
         gecko.is_hungry = False
         gecko.can_breed = True
-        print("{name} has a full belly now/n".format(name = gecko.name))
-
-
-# show_collection() is working
+        print("{name} has a full belly now\n".format(name = gecko.name))
 
   def show_collection(self):
     for gecko in gecko_collection:
       print(str(gecko) + "\n")
     menu()
 
-player_1 = Player("Ethan")
-#print(player_1)
+# add_xp() is adding xp for every gecko in gecko_collection, not just the new gecko.
+# I think I need to add this function to the breed function so it only add xp for the new gecko
+  def add_xp(self):
+    for gecko in gecko_collection:
+      if gecko.morph in exotic_morph_list:
+        player_1.experience += 10
+      else:
+        player_1.experience += 1
+
 
 print("""     
  ██████╗ ███████╗ ██████╗██╗  ██╗ ██████╗ 
@@ -130,40 +123,45 @@ print("""
 ██║  ███╗█████╗  ██║     █████╔╝ ██║   ██║
 ██║   ██║██╔══╝  ██║     ██╔═██╗ ██║   ██║
 ╚██████╔╝███████╗╚██████╗██║  ██╗╚██████╔╝
- ╚═════╝ ╚══════╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ """)
+ ╚═════╝ ╚══════╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ \n\n\n""")
 
-gecko_1 = Gecko(random.choice(names_list), "male", random.choice(all_morph_list))
-gecko_1 = Gecko(random.choice(names_list), "female", random.choice(all_morph_list))
+player_1 = Player(input("Hello! What is your name?: "))
+print("\nHello, {name}!\n".format(name = player_1.name))
+gecko1 = Gecko(input("Choose your male gecko's name: "), "male", random.choice(all_morph_list))
+gecko2 = Gecko(input("Choose your female gecko's name: "), "female", random.choice(all_morph_list))
 
-print("""Welcome to Gecko!\n
+print("""\nWelcome to Gecko!\n
 You have been given a pair of geckos to breed.
 To get started, select from the following options:\n""")
 
 def menu():
-  option1 = "1 - Breed geckos\n"
-  option2 = "2 - Feed gecko\n"
-  option3 = "3 - See gecko collection\n"
-  option4 = "4 - See player points\n"
-  print(option1, option2, option3, option4)
+  option1 = " 1 - Breed Geckos\n"
+  option2 = "2 - Feed Gecko\n"
+  option3 = "3 - See Gecko Collection\n"
+  option4 = "4 - See Player Stats\n"
+  option5 = "5 - Quit Game\n"
+  print(option1, option2, option3, option4, option5)
   selection = input("Select an option: ")
   if selection == "1":
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     player_1.breed()
     menu()
   elif selection == "2":
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     player_1.feed()
     menu()
   elif selection == "3":
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     player_1.show_collection()
     menu()
   elif selection == "4":
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print(player_1)
     menu()
-
-
-gecko1 = Gecko(random.choice(names_list), "male", random.choice(all_morph_list))
-gecko2 = Gecko(random.choice(names_list), "female", random.choice(all_morph_list))
-gecko3 = Gecko(random.choice(names_list), "male", random.choice(all_morph_list))
-gecko4 = Gecko(random.choice(names_list), "female", random.choice(all_morph_list))
+  elif selection == "5":
+    quit()
+  else:
+    menu()
 
 menu()
 
